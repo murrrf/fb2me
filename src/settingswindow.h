17327c47fc836c9417f6b773483a3862caac5582
Sergej Martynov <sergejmartynov@gmail.com>
2016-01-07 15:00:54 +0300

    Added template for settings dialog

---
+++ src/settingswindow.h
@@ -0,0 +1,57 @@
+/***********************************************************************
+ *
+ * Copyright (C) 2015 Veter <veter@veter.name>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ***********************************************************************/
+
+#ifndef SETTINGSWINDOW_H
+#define SETTINGSWINDOW_H
+
+/**
+ * @file
+ * @~russian
+ * @brief Модуль окна настроек.
+ *
+ * @~english
+ * @brief The module of the settings window.
+ */
+
+#include <QDialog>
+
+/**
+ * @~russian
+ * @brief Класс окна настроек.
+ *
+ * @~english
+ * @brief The SettingsWindow class.
+ */
+class SettingsWindow : public QDialog
+{
+    Q_OBJECT
+public:
+    /**
+     * @~russian
+     * @brief Конструктор окна настроек.
+     * @param parent Указатель на родительское окно.
+     *
+     * @~english
+     * @brief The settings window constructor.
+     * @param parent Parent window pointer.
+     */
+    SettingsWindow(QWidget *parent = 0);
+};
+
+#endif // SETTINGSWINDOW_H
